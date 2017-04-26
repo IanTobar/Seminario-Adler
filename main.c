5b0258caa678d5d0abcea13307289402f43e4ff9
Unknown <ruanbento@gmail.com>
2017-04-26 15:46:49 -0300

    Adicionado Comentarios a Main

--- main.c
+++ main.c
@@ -12,34 +12,49 @@ int main(int argc, char* argv[]) {
     /* First argument is executable name only */
     printf("\nexe name=%s", argv[0]);
 
+    //For usado somente para testes
     for (i = 1; i < argc; i++) {
         printf("\narg%d=%s", i, argv[i]);
     }
-
+    
     printf("\n");
+    //O comando atoi converte uma string em um valor int
     tamVet = atoi(argv[2]);
+    //Alocação de vetor de 5 espaços de inteiros
     tempo = (int*) malloc(5 * sizeof (int));
+    
+    //Checa se o vetor foi alocado adequadamente, caso retorne null
+    //o usuário será avisado que a memoria é insuficiente para a alocação
+    //e fechará o programa
     if (tempo == NULL) {
         printf("Memoria Insuficiente para alocar vetor de tempo\n");
         return 1;
     }
+    
+    //Aloca vetor dinamicamente
     troca = (unsigned long long int*) malloc(5 * sizeof (unsigned long long int));
+    //Checa se possui memoria para a alocagem
     if (troca == NULL) {
         printf("Memoria Insuficiente para alocar vetor de troca\n");
         return 1;
     }
+    //Aloca vetor dinamicamente
     comparacao = (unsigned long long int*) malloc(5 * sizeof (unsigned long long int));
+    //Checa se possui memoria para a alocagem
     if (comparacao == NULL) {
         printf("Memoria Insuficiente para alocar vetor de comparacao\n");
         return 1;
     }
+    //O comando atoi converte uma string em um valor int
     op = atoi(argv[1]);
+    //Se op é 1 ele ira utilizar o metodo de ordenação selection sempre pegando 
+    //seu tempo de execução que serão usados para gerar relatorio
     if (op == 1) {
         for (i = 3; i < 8; i++) {
             vet = leArquivo(argv[i], tamVet);
-            gettimeofday(&inicio, NULL);
+            gettimeofday(&inicio, NULL);//Pega o tempo inicial de execução
             selection(vet, tamVet);
-            gettimeofday(&final, NULL);
+            gettimeofday(&final, NULL);//Pega o tempo final de execução
             tempo[i - 3] = calculaTempoMili(inicio, final);
             if (i - 3 == 0) {
                 troca[i - 3] = contaTrocas() - 1;
@@ -53,6 +68,8 @@ int main(int argc, char* argv[]) {
         }
 
     } else {
+        //Se op é 2 ele ira utilizar o metodo de ordenação rapidSort sempre pegando 
+        //seu tempo de execução que serão usados para gerar relatorio
         if (op == 2) {
             for (i = 3; i < 8; i++) {
                 vet = leArquivo(argv[i], tamVet);
